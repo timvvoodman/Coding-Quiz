58446afa95a56519d6dacec7b46a6005eb5710d3
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2020-10-19 23:08:25 -0400

    can add score to local storage but cannot get back out into score list

--- scrpit.js
+++ scrpit.js
@@ -1,11 +1,15 @@
 //defaul page load displays Title of coding quiz and a start button
 
-// the first question diaplays and
-const startButton = document.getElementById("start-btn");
+const startButton = document.getElementById("controls");
 const questionContainerEl = document.getElementById("question-container");
 const questionEl = document.getElementById("question");
 const answerButtonsEL = document.getElementById("answer-buttons");
 const rightWrong = document.getElementById("alertUser");
+const scoreContainer = document.getElementById("score-container");
+const finalScore = document.getElementById("score");
+const submitScore = document.getElementById("submitHighScore");
+const clearScore = document.getElementById("clear");
+let scoreList = document.getElementById("score-list");
 
 let listQuestions, currentQuestionIndex, button;
 let score = 0;
@@ -21,13 +25,24 @@ function startQuiz() {
   questionContainerEl.classList.remove("hide");
   nextQuestion();
 }
+
+//function to end Quiz
+function endQuiz() {
+  startButton.classList.add("hide");
+  questionContainerEl.classList.add("hide");
+  scoreContainer.classList.remove("hide");
+  rightWrong.classList.add("hide");
+}
+
 // a timer starts for 60 seconds
+// 10 seconds removed for wrong answer?????????????????????????????????
 function startTimer() {
   console.log("timer started");
-  let sec = 5;
+  let sec = 30;
   const timer = setInterval(function () {
     if (sec === 0) {
       clearInterval(timer);
+      endQuiz();
     }
     document.getElementById("time-display").innerHTML = "00:" + sec;
     sec--;
@@ -58,29 +73,65 @@ function resetQuestions() {
 }
 
 //2. When the user clicks (selects) an answer
-// 10 seconds removed for wrong answer
-
 function selectAnswer(e) {
+  //the score is updated by 20 for each correct answer, not updated is wrong
+  // unerneath the next question the text displays "correct" or "wrong" based on the users selection on the previous question
+  if (e.target.value === "true") {
+    rightWrong.innerText = "Correct!";
+    score += 20;
+    console.log(score);
+  } else {
+    rightWrong.innerText = "Wrong!";
+  }
   //moves to next question automatically
   if (currentQuestionIndex <= 3) {
     currentQuestionIndex++;
     nextQuestion();
+  } else if ((currentQuestionIndex = 4)) {
+    // the quiz score is displayed
+
+    endQuiz();
+    finalScore.innerHTML =
+      " Your score is " + score + ". Enter your intials for the leaderboard!";
   } else {
     nextQuestion();
   }
-
-  if (e.target.value === "true") {
-    rightWrong.innerText = "Correct!";
-    score += 20;
-    document.getElementById("score").innerHTML = score;
-  } else {
-    rightWrong.innerText = "Wrong!";
-  }
-  console.log(score);
 }
-//the score is updated by 20 for each correct answer, not updated is wrong
 
-// 10 seconds removed for wrong answer
+// user is asked to enter their initials on a form for the high score list
+submitScore.addEventListener("click", saveScore);
+
+function saveScore(event) {
+  event.preventDefault();
+  let initials = document.getElementById("initials").value;
+  let scoreArray = JSON.parse(window.localStorage.getItem("scoreArray")) || [];
+  let scoreObject = {
+    score: score,
+    initials: initials,
+  };
+  scoreArray.push(scoreObject);
+
+  window.localStorage.setItem("scoreArray", JSON.stringify(scoreArray));
+
+  for (let i = 0; i < scoreArray.length; i++) {
+    let listItem = document.createElement("li");
+
+    listItem.appendChild(
+      document.createTextNode(initials.initials + score.score)
+    );
+    scoreList.appendChild(listItem);
+  }
+}
+
+// function printHighScores() {
+//   let scoreArray = JSON.parse(localStorage.getItem("scoreArray")) || [];
+//   //get initials, get score, create append html ul
+//   for (let i = 0; scoreArray.length - 1; i++) {
+//     let listItem = document.createElement("LI");
+//     let listText = document.createTextNode("hi");
+//     listItem.appendChild(listText);
+//     document.getElementById("score-list").appendChild(listItem);
+//   }
 
 const questions = [
   {
@@ -133,14 +184,10 @@ const questions = [
   },
 ];
 
-// unerneath the next question the text displays "correct" or "wrong" based on the users selection on the previous question
-//3. When an incorrect  question is answered 10 secons is removed from the timer
 //4. after the last question
-// clculate the score on the quiz
-// the quiz score is displayed
-// user is asked to enter their initials on a form for the high score list
+
 // submit button next to user form
-//submits user initials and scoore to high score list
+//submits user initials and score to high score list
 // updates page to diplay list of high scores
 //5. high score display
 //button to clear high scores
